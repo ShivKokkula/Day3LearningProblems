e482536e2ea27b87dd85addf20519e694e8de87d
ShivKokkula <shivbk987@gmail.com>
2021-05-22 18:45:28 +0530

    Refactor- added employ check function in the program

--- EmpWage.java
+++ EmpWage.java
@@ -2,9 +2,27 @@ package EmpWage;
 
 public class EmpWage {
 
+	static final int IS_FULL_TIME = 1;
+
 	public static void main(String[] args) {
 		
 		System.out.println("Welcome to Employee Wage Computation");
+		
+		EmpWage e1 = new EmpWage();
+		e1.empCheck();
 	}
+	
+	public void empCheck() {
+		
+		int empCheck = (int) (Math.floor(Math.random() * 10) % 2);
+		
+		if (empCheck == IS_FULL_TIME) {
+			System.out.println("Employ Present");
+		}else {
+			
+			System.out.println("Employ Absent");
+		}
+	}
+	
 
 }
