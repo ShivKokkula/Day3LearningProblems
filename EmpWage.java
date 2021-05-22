93bcdda8f3e5d7464ffff5c3d3e77cd54ca33e95
ShivKokkula <shivbk987@gmail.com>
2021-05-22 20:02:39 +0530

    Refactor - created seperate function for getting monthly hours

--- EmpWage.java
+++ EmpWage.java
@@ -5,13 +5,14 @@ public class EmpWage {
 	static final int IS_FULL_TIME = 1;
 	static final int IS_PART_TIME = 2;
 	static final int EMP_RATE_PER_HOUR = 20;
+	public static final int NUM_OF_WORKING_DAYS = 2;
 
 	public static void main(String[] args) {
 		
 		System.out.println("Welcome to Employee Wage Computation");
 		
 		EmpWage e1 = new EmpWage();
-		double wage = e1.calDailyWage(e1.empCheck());
+		double wage = e1.calDailyWage(e1.empHrsForMonth());
 		
 		System.out.println("Employee Wage: "+wage);
 	}
@@ -39,6 +40,15 @@ public class EmpWage {
 		}
 		
 		return empHrs;
+	}
+	
+	public int empHrsForMonth() {
+		int totalEmpHrs = 0;
+		for (int day =0; day<NUM_OF_WORKING_DAYS; day++) {
+			totalEmpHrs = totalEmpHrs + empCheck();
+		}
+		
+		return totalEmpHrs;
 	}
 	
 	public double calDailyWage(int empHrs) {
