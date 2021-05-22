5d95ce840ea3bd12dc57d5a57f4b988f79c379de
ShivKokkula <shivbk987@gmail.com>
2021-05-22 20:26:52 +0530

    Refactor - added max hours in month limit in the program

--- EmpWage.java
+++ EmpWage.java
@@ -5,7 +5,9 @@ public class EmpWage {
 	static final int IS_FULL_TIME = 1;
 	static final int IS_PART_TIME = 2;
 	static final int EMP_RATE_PER_HOUR = 20;
-	public static final int NUM_OF_WORKING_DAYS = 2;
+	public static final int NUM_OF_WORKING_DAYS = 4;
+	public static final int MAX_HOURS_IN_MONTH = 20;
+	int totalWorkingDays = 0;
 
 	public static void main(String[] args) {
 		
@@ -14,7 +16,7 @@ public class EmpWage {
 		EmpWage e1 = new EmpWage();
 		double wage = e1.calDailyWage(e1.empHrsForMonth());
 		
-		System.out.println("Employee Wage: "+wage);
+		System.out.println("Total Employee Wage: "+wage);
 	}
 	
 	public int empCheck() {
@@ -25,16 +27,16 @@ public class EmpWage {
 		
 		switch(empCheck) {
 		case IS_FULL_TIME:
-			System.out.println("Employ Present: Full Time");
+			System.out.println("Day "+(totalWorkingDays+1)+" Employ Present: Full Time");
 			empHrs = 8;
 			break;
 		case IS_PART_TIME:
-			System.out.println("Employ Present: Part Time");
+			System.out.println("Day "+(totalWorkingDays+1)+" Employ Present: Part Time");
 			empHrs = 4;
 			break;
 			
 		default:
-			System.out.println("Employ Absent");
+			System.out.println("Day "+(totalWorkingDays+1)+" Employ Absent");
 			empHrs = 0;
 			break;
 		}
@@ -44,8 +46,9 @@ public class EmpWage {
 	
 	public int empHrsForMonth() {
 		int totalEmpHrs = 0;
-		for (int day =0; day<NUM_OF_WORKING_DAYS; day++) {
+		while (totalEmpHrs < MAX_HOURS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS ) {
 			totalEmpHrs = totalEmpHrs + empCheck();
+			totalWorkingDays++;
 		}
 		
 		return totalEmpHrs;
