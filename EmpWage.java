22884ee0f99afe2e134782c20473a4f4d25172b3
ShivKokkula <shivbk987@gmail.com>
2021-05-22 19:08:58 +0530

    Refactor - added daily wage calculation function in the program

--- EmpWage.java
+++ EmpWage.java
@@ -3,25 +3,40 @@ package EmpWage;
 public class EmpWage {
 
 	static final int IS_FULL_TIME = 1;
+	static final int EMP_RATE_PER_HOUR = 20;
 
 	public static void main(String[] args) {
 		
 		System.out.println("Welcome to Employee Wage Computation");
 		
 		EmpWage e1 = new EmpWage();
-		e1.empCheck();
+		double wage = e1.calDailyWage(e1.empCheck());
+		
+		System.out.println("Employee Wage: "+wage);
 	}
 	
-	public void empCheck() {
+	public int empCheck() {
+		
+		int empHrs = 0;
 		
 		int empCheck = (int) (Math.floor(Math.random() * 10) % 2);
 		
 		if (empCheck == IS_FULL_TIME) {
 			System.out.println("Employ Present");
+			empHrs = 8;
 		}else {
 			
 			System.out.println("Employ Absent");
 		}
+		
+		return empHrs;
+	}
+	
+	public double calDailyWage(int empHrs) {
+		double empWage = 0;
+		empWage = empHrs * EMP_RATE_PER_HOUR;
+		
+		return empWage;
 	}
 	
 
